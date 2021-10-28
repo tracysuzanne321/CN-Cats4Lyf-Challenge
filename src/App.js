d942643c1c3bc53f6eee30c6d3172f32050a3e0d
asnahassan <65673886+asnahassan@users.noreply.github.com>
2021-10-28 13:34:13 +0200

    added hero and footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { useEffect, useState } from 'react';
 import './App.css';
 import fetchImg from './utils/fetchImg';
 import fetchData from './utils/fetchData';
+import Hero from "./utils/hero"
+import Footer from './utils/footer';
 
 const App = () => {
   const [catImgArr, setCatImgArr] = useState([]);
@@ -21,12 +23,14 @@ const App = () => {
     <>
       <div>
         <h1>Cats4Lyf</h1>
+        <Hero />
         {catImgArr.map((cat, i) => (
           <div id={cat.id} key={cat.id}>
             <img className="img" alt="" src={cat.url} />
             <span>{catDataArr[i]?.name}</span>
           </div>
         ))}
+        <Footer />
       </div>
     </>
   );
---
+++ src/utils/footer.js
@@ -0,0 +1,12 @@
+const Footer = () => {
+    return (
+        <>
+        <p>Copyright © 2021 Cats4Lyf - All Rights Reserved.</p>
+        <a href="">PRIVACY POLICY </a>
+        <a href="">TERMS AND CONDITIONS</a>
+        </>
+    )
+}
+
+
+export default Footer
