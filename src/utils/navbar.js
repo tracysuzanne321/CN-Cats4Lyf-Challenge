2da6eeae5d77302755083df87f8b37643e17f75a
James Arnold <j1mesu@hotmail.co.uk>
2021-10-29 12:34:54 +0100

    save update

--- src/utils/navbar.js
+++ src/utils/navbar.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import { IconContext } from 'react-icons';
-import { AiOutlineShoppingCart } from 'react-icons/ai';
+import React from "react";
+import { IconContext } from "react-icons";
+import { AiOutlineShoppingCart } from "react-icons/ai";
 
 const Navbar = ({ handleOpen }) => {
   return (
     <div className="shop-cart">
       <IconContext.Provider
-        value={{ color: 'black', size: '25px', marginLeft: '100px' }}
+        value={{ color: "black", size: "25px", marginLeft: "100px" }}
       >
         <div onClick={handleOpen} className="shop-cart">
           <AiOutlineShoppingCart />
         </div>
       </IconContext.Provider>
+      <span>{count}in basket</span>
     </div>
   );
 };
